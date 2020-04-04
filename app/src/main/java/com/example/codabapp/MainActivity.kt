c7e40d7cd03ea44a4086f88df54f55db7c062ac1
Mariana Moreno <mariana.anaya@hotmail.com>
2020-04-04 16:11:04 -0500

    audio recorder

--- app/src/main/java/com/example/codabapp/MainActivity.kt
+++ app/src/main/java/com/example/codabapp/MainActivity.kt
@@ -9,13 +9,27 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_pedidos.*
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mr : MediaRecorder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var path = Environment.getExternalStorageDirectory().toString()+ "/recordPedido.3gp"
+        mr = MediaRecorder()
+        buttonStart.isEnabled = false
+        buttonStop.isEnabled = false
+
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
@@ -27,5 +41,47 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_pedidos, R.id.navigation_tienda, R.id.navigation_clientes))
         // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest. permission.RECORD_AUDIO,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
+            buttonStart.isEnabled = true
+
+        // Start Recording
+        buttonStart.setOnClickListener {
+            mr.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mr.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+            mr.setOutputFile(path)
+            mr.prepare()
+            mr.start()
+            buttonStop.isEnabled =true
+            buttonStart.isEnabled =false
+        }
+
+        //Stop recording
+        buttonStop.setOnClickListener {
+            mr.stop()
+            buttonStart.isEnabled = true
+            buttonStop.isEnabled = false
+        }
+        // Play recording
+        buttonPlay.setOnClickListener {
+            var mp = MediaPlayer()
+            mp.setDataSource(path)
+            mp.prepare()
+            mp.start()
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode===111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            buttonStart.isEnabled = true
     }
 }
--- app/src/main/java/com/example/codabapp/ui/pedidos/DetallePedidoFragment.kt
+++ app/src/main/java/com/example/codabapp/ui/pedidos/DetallePedidoFragment.kt
@@ -1,12 +1,15 @@
 package com.example.codabapp.ui.pedidos
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 
 import com.example.codabapp.R
+import java.util.jar.Manifest
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,13 +32,16 @@ class DetallePedidoFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_detalle_pedido, container, false)
     }
 
